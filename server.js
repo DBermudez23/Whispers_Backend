95e1b631ee7ae4f0c1cd0efc1447bd2e37fb6964
Daniel <danielbermudezwuw@gmail.com>
2025-06-14 15:45:58 -0500

    Lint issue GHA

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import cors from 'cors' 
+import cors from 'cors'
 import bodyParser from 'body-parser'
 import * as whisper from './stores/whisper.js'
 import * as user from './stores/user.js'
